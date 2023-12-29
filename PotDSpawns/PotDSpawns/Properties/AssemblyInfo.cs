a0d36898bd4209470717f97b26503aff418c7e31
v1ld <v1ld.git@gmail.com>
2023-12-29 18:47:16 -0500

    Add PotDSpawns mod.

---
+++ PotDSpawns/PotDSpawns/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using Patchwork.Attributes;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PotDSpawns")]
+[assembly: PatchAssembly]
+[assembly: AssemblyDescription("PotD spawns in other difficulties for Pillars of Eternity 1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PotDSpawns")]
+[assembly: AssemblyCopyright("Copyright ©v1ld, Dec 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76F2E0C7-A116-446F-96B7-B0C64310F9E1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PotDSpawns/PotDSpawns/mod_PotDSpawns.cs
@@ -0,0 +1,54 @@
+﻿using Patchwork.Attributes;
+using UnityEngine;
+
+namespace PotDSpawns
+{
+    [ModifiesType]
+    public class v1ld_Encounter : Encounter
+    {
+        [ModifiesMember("DoesSpawnAppear")]
+        new public bool DoesSpawnAppear(DifficultySettings settings)
+        {
+            if (settings.RequiresAnyOf != 0 && !DifficultyScaling.Instance.IsAnyScalerActive(settings.RequiresAnyOf))
+            {
+                return false;
+            }
+            GameDifficulty difficulty = GameDifficulty.Normal;
+            if (GameState.Instance.CurrentNextMap != null)
+            {
+                difficulty = GameState.Instance.CurrentNextMap.StoryTimeSpawnSetting;
+            }
+            else
+            {
+                Debug.LogWarning("CurrentNextMap was null in encounter spawning.", this);
+            }
+            GameDifficulty savedDifficulty = SavedDifficulty;
+            if (savedDifficulty == GameDifficulty.StoryTime && settings.AppearsInBaseDifficulty(difficulty))
+            {
+                return true;
+            }
+            switch (savedDifficulty)
+            {
+                case GameDifficulty.Normal:
+                case GameDifficulty.Hard:
+                case GameDifficulty.PathOfTheDamned:
+                    return true;
+                case GameDifficulty.Easy:
+                    if (settings.Easy)
+                    {
+                        return true;
+                    }
+                    break;
+            }
+            if (savedDifficulty == GameDifficulty.Normal && settings.Normal)
+            {
+                return true;
+            }
+            if (savedDifficulty == GameDifficulty.Hard && settings.Hard)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
