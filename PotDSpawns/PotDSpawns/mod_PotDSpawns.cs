dbda8134cbce20c83a483cff7fac1e81e5fa2f04
v1ld <v1ld.git@gmail.com>
2023-12-30 09:11:37 -0500

    No PotD spawns for Normal, just Hard.

--- PotDSpawns/PotDSpawns/mod_PotDSpawns.cs
+++ PotDSpawns/PotDSpawns/mod_PotDSpawns.cs
@@ -9,45 +9,35 @@ namespace PotDSpawns
         [ModifiesMember("DoesSpawnAppear")]
         new public bool DoesSpawnAppear(DifficultySettings settings)
         {
-            if (settings.RequiresAnyOf != 0 && !DifficultyScaling.Instance.IsAnyScalerActive(settings.RequiresAnyOf))
-            {
+            if (settings.RequiresAnyOf != 0 && !DifficultyScaling.Instance.IsAnyScalerActive(settings.RequiresAnyOf)) {
                 return false;
             }
+
             GameDifficulty difficulty = GameDifficulty.Normal;
-            if (GameState.Instance.CurrentNextMap != null)
-            {
+            if (GameState.Instance.CurrentNextMap != null) {
                 difficulty = GameState.Instance.CurrentNextMap.StoryTimeSpawnSetting;
-            }
-            else
-            {
+            } else {
                 Debug.LogWarning("CurrentNextMap was null in encounter spawning.", this);
             }
+
             GameDifficulty savedDifficulty = SavedDifficulty;
-            if (savedDifficulty == GameDifficulty.StoryTime && settings.AppearsInBaseDifficulty(difficulty))
-            {
+            if (savedDifficulty == GameDifficulty.StoryTime && settings.AppearsInBaseDifficulty(difficulty)) {
                 return true;
             }
-            switch (savedDifficulty)
-            {
-                case GameDifficulty.Normal:
-                case GameDifficulty.Hard:
-                case GameDifficulty.PathOfTheDamned:
-                    return true;
-                case GameDifficulty.Easy:
-                    if (settings.Easy)
-                    {
-                        return true;
-                    }
-                    break;
-            }
-            if (savedDifficulty == GameDifficulty.Normal && settings.Normal)
-            {
+
+            // v1ld: Hard gets all the spawns, same as PotD
+            if (savedDifficulty == GameDifficulty.PathOfTheDamned || savedDifficulty == GameDifficulty.Hard) {
                 return true;
             }
-            if (savedDifficulty == GameDifficulty.Hard && settings.Hard)
-            {
+            if (savedDifficulty == GameDifficulty.Easy && settings.Easy) {
                 return true;
             }
+            if (savedDifficulty == GameDifficulty.Normal && settings.Normal) {
+                return true;
+            }
+            //if (savedDifficulty == GameDifficulty.Hard && settings.Hard) {
+            //    return true;
+            //}
             return false;
         }
     }
