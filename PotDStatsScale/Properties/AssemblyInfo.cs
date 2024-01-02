4f8c01b7fe837499cb11e3bdc807e041f54965fd
v1ld <v1ld.git@gmail.com>
2024-01-02 08:17:22 -0500

    Add PotD stats scaling.

---
+++ PotDStatsScale/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using Patchwork.Attributes;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PotDStatsScale")]
+[assembly: PatchAssembly]
+[assembly: AssemblyDescription("PotD NPC stats changes scale with level")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PotDStatsScale")]
+[assembly: AssemblyCopyright("Copyright ©v1ld, Dec 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2D8FB1D0-7D07-4051-B89F-0364479016A0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PotDStatsScale/mod_PotDStatsScale.cs
@@ -0,0 +1,77 @@
+﻿using Patchwork.Attributes;
+
+namespace PotDStatsScale
+{
+    [ModifiesType]
+    public class  V1ldCharacterStats : CharacterStats
+    {
+        [NewMember]
+        public int V1ldScaleLevelStart
+        { 
+            get { return 1; }
+        }
+
+        [NewMember]
+        public int V1ldScaleLevelEnd
+        {
+            get { return 12; }
+        }
+
+        [NewMember]
+        public int V1ldScaleAccDefMin
+        {
+            get { return 5; }
+        }
+
+        [NewMember]
+        public int V1ldScaleAccDefMax
+        {
+            get { return 15; }
+        }
+
+        [ModifiesMember("DifficultyStatBonus")]
+        new public float DifficultyStatBonus
+        {
+            get
+            {
+                if ((bool)GameState.Instance && GameState.Instance.IsDifficultyPotd && !IsPartyMember)
+                {
+                    if (Level <= V1ldScaleLevelStart)
+                    {
+                        return V1ldScaleAccDefMin;
+                    }
+                    else if (Level >= V1ldScaleLevelEnd)
+                    {
+                        return V1ldScaleAccDefMax;
+                    }
+                    else
+                    {
+                        return V1ldScaleAccDefMin + (V1ldScaleAccDefMax - V1ldScaleAccDefMin) * (Level - V1ldScaleLevelStart) / (V1ldScaleLevelEnd - V1ldScaleLevelStart);
+                    }
+                }
+                return 0f;
+            }
+        }
+
+        //[ModifiesMember("DifficultyHealthStaminaMult")]
+        //new public float DifficultyHealthStaminaMult
+        //{
+        //    get
+        //    {
+        //        if (!IsPartyMember && (IsPartyMember || !HasFactionSwapEffect()) && (bool)GameState.Instance)
+        //        {
+        //            if (GameState.Instance.IsDifficultyPotd)
+        //            {
+        //                return 1.25f;
+        //            }
+        //            if (GameState.Instance.IsDifficultyStoryTime)
+        //            {
+        //                return 0.5f;
+        //            }
+        //            return 1f;
+        //        }
+        //        return 1f;
+        //    }
+        //}
+    }
+}
