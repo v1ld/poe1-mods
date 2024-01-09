dd27ce7d1100ca32ac995777b008c9fd4479e1a2
v1ld <v1ld.git@gmail.com>
2024-01-09 16:06:04 -0500

    Add DetectHiddenObjectsWithoutStealth mod.

---
+++ DetectHiddenObjectsWithoutStealth/mod_DetectHiddenObjectsWithoutStealth.cs
@@ -0,0 +1,24 @@
+﻿using Patchwork.Attributes;
+
+namespace V1ldDetectHiddenObjectsWithoutStealth
+{
+    [ModifiesType]
+    class V1ld_CharacterStatsDHOWS: CharacterStats
+    {
+		[ModifiesMember("DetectionRange")]
+        new public float DetectionRange(Detectable d)
+        {
+            int detectionRange = CalculateSkill(SkillType.Mechanics);
+            //if (!Stealth.IsInStealthMode(base.gameObject))
+            //{
+            //    detectionRange -= 4;
+            //}
+            detectionRange += 1;
+            if (d != null)
+            {
+                detectionRange -= d.GetDifficulty();
+            }
+            return detectionRange;
+        }
+    }
+}
